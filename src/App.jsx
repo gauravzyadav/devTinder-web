96cf456d626aae1f0df729f10e53cff547b5cd00
Gaurav Yadav <gauravyadav9182@gmail.com>
2025-06-21 00:48:53 +0530

    added login

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
- import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+
+import Body from "./components/Body";
+import Landing from "./components/LandingPage";
+import Feed from "./components/Feed";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <>
-
+    <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
-        <Route path="/" element={<Body/>}>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/profile" element={<Profile />}/>
-        </Route>
+          {/* Shared layout with NavBar & Footer */}
+          <Route path="/" element={<Body />}>
+            
+            <Route path="login" element={<Login />} />
+            <Route path="feed" element={<Feed />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
 export default App;
- 
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const LandingPage = () => {
+    const navigate = useNavigate();
+
+    const changeTo = () =>{
+         navigate("/login")
+    }
+
+  return (
+    <div
+      className="h-screen w-full bg-cover bg-center relative"
+      style={{
+        backgroundImage: "url('https://marjon-journalism.shorthandstories.com/tinder-how-they-keep-you-swiping/assets/SgbRM44jay/tinder-gradient-750x422.png')", // Replace with your actual image path or URL
+      }}
+    >
+      {/* <div className="absolute top-3 left-5 text-white text-4xl font-dev">
+        DevTinder
+      </div> */}
+      
+      <div className="absolute top-5 right-5">
+        <button className="btn btn-primary" onClick={changeTo}>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
