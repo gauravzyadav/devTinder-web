965ed31d99fbd1a4ef7e031adeac3cd4290c4366
Gaurav Yadav <gauravyadav9182@gmail.com>
2025-06-22 02:32:32 +0530

    accept/reject

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -2,11 +2,22 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 const Request = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {}
+  };
 
   const fetchRequests = async () => {
     try {
@@ -14,8 +25,7 @@ const Request = () => {
         withCredentials: true,
       });
 
-      dispatch(addRequests((await res).data.data))
-
+      dispatch(addRequests((await res).data.data));
     } catch (err) {
       //
     }
@@ -23,13 +33,12 @@ const Request = () => {
 
   useEffect(() => {
     fetchRequests();
-  }, [])
-
+  }, []);
 
   if (!requests) return;
 
   if (requests.length == 0)
-    return <h1 className="text-bold text-2xl">No Request Found</h1>;
+    return <h1 className="text-bold text-2xl flex justify-center my-10">No Request Found</h1>;
 
   return (
     <div className=" text-center my-5 ">
@@ -40,9 +49,16 @@ const Request = () => {
           request.fromUserId;
 
         return (
-          <div key ={_id} className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/4 mx-auto">
+          <div
+            key={_id}
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/4 mx-auto"
+          >
             <div>
-              <img alt="photo" className="w-20 h-20 rounded-full" src={photoUrl} />
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
             </div>
             <div className="text-left mx-4">
               <h2 className="font-bold">{firstName + " " + lastName}</h2>
@@ -50,8 +66,8 @@ const Request = () => {
               <p>{about}</p>
             </div>
             <div>
-            <button class="btn btn-primary mx-2">Reject</button>
-            <button class="btn btn-secondary mx-2">Accept</button>
+              <button className="btn btn-primary mx-2" onClick={() => reviewRequest("rejected", request._id)}>Reject</button>
+              <button className="btn btn-secondary mx-2" onClick={() => reviewRequest("accepted", request._id)}>Accept</button>
             </div>
           </div>
         );
