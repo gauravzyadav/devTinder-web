a75edaae4e883ac67b889d587b223d1b5dcfc4d0
Gaurav Yadav <gauravyadav9182@gmail.com>
2025-06-21 11:46:59 +0530

    Building Logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("gaurav@gmail.com");
   const [password, setPassword] = useState("Gaurav@123");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -23,6 +24,7 @@ const Login = () => {
        dispatch(addUser(res.data));
        return navigate("/")
     } catch (err) {
+      setError(err?.response?.data || "Something went wrong" );
       console.error(err);
     }
   };
@@ -57,7 +59,7 @@ const Login = () => {
               />
             </label>
           </div>
-
+          <p className="text-red-500"> {error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
