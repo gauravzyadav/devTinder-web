531802a22bf47365db2287a5dbf375a0d1af2a72
gauravzyadav <gauravyadav9182@gmail.com>
2025-06-28 15:02:31 +0530

    Dynamic URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,10 @@
-export const BASE_URL = "http://localhost:3000";
+//export const BASE_URL = "http://localhost:3000";
 
 // utils/constants.js
-//export const BASE_URL = "https://dev-tinder-backend-blue.vercel.app"
+// export const BASE_URL = "https://dev-tinder-backend-blue.vercel.app"
+
+
+export const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://dev-tinder-backend-blue.vercel.app";
